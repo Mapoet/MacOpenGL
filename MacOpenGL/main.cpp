6349d07a0e530a775feb9bf57133cc27fb33dd9b
Mapoet <Mapoet.Niphy@Gmail.com>
2018-03-31 14:10:01 +0800

    Initial Commit

---
+++ MacOpenGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MacOpenGL
+//
+//  Created by Mapoet Niphy on 2018/3/31.
+//  Copyright © 2018年 Mapoet Niphy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
